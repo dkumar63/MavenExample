07caf712918ea6f0133c36835360226d73c96ce5
ibm_admin <ibm_admin@ADMINIB-U0RGV7D.in.ibm.com>
2017-05-04 20:31:07 +0530

    Signed-off-by: ibm_admin <ibm_admin@ADMINIB-U0RGV7D.in.ibm.com>

---
+++ src/test/java/mavan/examplecom/Example.java
@@ -0,0 +1,12 @@
+package mavan.examplecom;
+
+import org.testng.annotations.Test;
+
+public class Example
+{
+	@Test
+	public void printName()
+	{
+		System.out.println("Welcome Maven to Deepak World :) ");
+	}
+}
